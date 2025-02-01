e12e23bf09e07a7730dbc72a2da03438d7f03a4d
Kashish Mandhane <148366732+KashProgramming@users.noreply.github.com>
2025-02-01 14:36:42 +0530

    Delete content_processor/table_processor.py

--- content_processor/table_processor.py
+++
@@ -1,7 +0,0 @@
-def filter_tables(list_of_df):
-    # add logic for filtering which tables to keep and which to discard
-    filtered_tables = []
-    for df in list_of_df:
-        if len(df) > 1:
-            filtered_tables.append(df)
-    return filtered_tables
